46249f8b31666ca305d58935496ba6921c552d4d
Spyros Ligouras <sidon19@gmail.com>
2013-11-13 14:54:15 +0200

    split initialization from execution script

---
+++ init.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd /etc/nginx/sites-enabled && rm default
+
+cd /etc/nginx/sites-available && cat << EOF > ghost
+server {
+    listen 0.0.0.0:80;
+    server_name ghost;
+    access_log /var/log/nginx/ghost.log;
+
+    location / {
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header HOST \$http_host;
+        proxy_set_header X-NginX-Proxy true;
+
+        proxy_pass http://127.0.0.1:2368;
+        proxy_redirect off;
+    }
+}
+EOF
+
+cd /etc/nginx/sites-enabled && ln -sf ../sites-available/ghost ghost
+
+/etc/init.d/nginx reload
+
+sed -i "s,^/usr/local/bin/init.sh,#/usr/local/bin/init.sh," /usr/local/bin/run.sh
